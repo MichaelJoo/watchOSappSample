447bfa449534d5b7999f489f0b7f30a7f66cc42a
OnepieceMJ <34578674+MichaelJoo@users.noreply.github.com>
2022-01-08 01:28:01 +0900

    Add files via upload

---
+++ Intermediates/ContentView.5.preview-thunk.swift
@@ -0,0 +1,23 @@
+@_private(sourceFile: "ContentView.swift") import WatchOSapp
+import SwiftUI
+import SwiftUI
+
+extension ContentView_Previews {
+    @_dynamicReplacement(for: previews) private static var __preview__previews: some View {
+        #sourceLocation(file: "/Users/dohyungjoo/Library/Mobile Documents/com~apple~CloudDocs/AppleDevelopment/WatchOSapp/WatchOSapp/ContentView.swift", line: 19)
+        __designTimeSelection(ContentView(), "#8344.[2].[0].property.[0].[0]")
+    #sourceLocation()
+    }
+}
+
+extension ContentView {
+    @_dynamicReplacement(for: body) private var __preview__body: some View {
+        #sourceLocation(file: "/Users/dohyungjoo/Library/Mobile Documents/com~apple~CloudDocs/AppleDevelopment/WatchOSapp/WatchOSapp/ContentView.swift", line: 12)
+        __designTimeSelection(Text(__designTimeString("#8344.[1].[0].property.[0].[0].arg[0].value", fallback: "Hello, Dohyung!"))
+            .padding(), "#8344.[1].[0].property.[0].[0]")
+    #sourceLocation()
+    }
+}
+
+import struct WatchOSapp.ContentView
+import struct WatchOSapp.ContentView_Previews
